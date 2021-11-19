b2369bc06f2c6daa2166c9944ba42ef28b7d17b4
Gabriel Schultz <gabriel.bridi2008@hotmail.com>
2021-11-19 07:31:36 -0300

    feat(storybook): updating storybook theme provider

--- projetos/ui/src/components/Button/index.tsx
+++ projetos/ui/src/components/Button/index.tsx
@@ -1,4 +1,21 @@
 import React from "react";
+import styled from 'styled-components';
+import get from 'lodash/get';
+
+const ButtonWrapper = styled.button`
+    border: 0;
+    cursor: pointer;
+    padding: 12px 26px;
+    font-weight: bold;
+    opacity: 1;
+    border-radius: 8px;
+    color: ${({ theme, color }) => get(theme, `colors.${color}.contrastText`)};
+    background: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.2;
+    }
+`;
 
 interface ButtonProps {
   /**
@@ -8,17 +25,22 @@ interface ButtonProps {
   /**
    * Adicione o que quiser renderizar no botão
    */
-  children: React.ReactNode
+  children: React.ReactNode,
+  /**
+   * Define a cor do botão
+   */
+  color: string,
 }
 
-export default function Button({ disabled, children }: ButtonProps){
+export default function Button({ disabled, children, color }: ButtonProps){
     return(
-        <button disabled={disabled}>
+        <ButtonWrapper disabled={disabled} color={color}>
           {children}
-        </button>
+        </ButtonWrapper>
     )
 }
 
 Button.defaultProps ={
-  disabled: true,
+  disabled: false,
+  color: 'primary.main'
 }
